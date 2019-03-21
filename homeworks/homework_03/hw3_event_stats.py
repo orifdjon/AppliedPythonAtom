c6e6187253799868d785e4699aff8e596cd7c8b9
Arif <orif95@mail.ru>
2019-03-21 09:06:52 +0300

    done event_stats

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -6,8 +6,7 @@ class TEventStats:
     FIVE_MIN = 300
 
     def __init__(self):
-        # TODO: реализовать метод
-        raise NotImplementedError
+        self.activity_cash = {}
 
     def register_event(self, user_id, time):
         """
@@ -16,8 +15,10 @@ class TEventStats:
         :param time: время (timestamp)
         :return: None
         """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        if user_id in self.activity_cash:
+            self.activity_cash[user_id].append(time)
+        else:
+            self.activity_cash[user_id] = [time]
 
     def query(self, count, time):
         """
@@ -28,5 +29,10 @@ class TEventStats:
         :param time: время для рассчета интервала
         :return: activity_count: int
         """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        count_of_people = 0
+        for user, _time in self.activity_cash.items():
+            if _time[0] <= time:
+                count_act = len(list(filter(lambda x: time - self.FIVE_MIN < x <= time, _time)))
+                if count_act == count:
+                    count_of_people += 1
+        return count_of_people
