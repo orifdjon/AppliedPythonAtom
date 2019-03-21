81684970c457ee275bccb11abcb9dae384b9babf
Arif <orif95@mail.ru>
2019-03-21 09:45:31 +0300

    final fix pep8

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -3,32 +3,21 @@
 
 
 class TEventStats:
+
     FIVE_MIN = 300
 
     def __init__(self):
         self.activity_cash = {}
 
     def register_event(self, user_id, time):
-        """
-        Этот метод регистрирует событие активности пользователя.
-        :param user_id: идентификатор пользователя
-        :param time: время (timestamp)
-        :return: None
-        """
+
         if user_id in self.activity_cash:
             self.activity_cash[user_id].append(time)
         else:
             self.activity_cash[user_id] = [time]
 
     def query(self, count, time):
-        """
-        Этот метод отвечает на запросы.
-        Возвращает количество пользователей, которые за последние 5 минут
-        (на полуинтервале времени (time - 5 min, time]), совершили ровно count действий
-        :param count: количество действий
-        :param time: время для рассчета интервала
-        :return: activity_count: int
-        """
+
         count_of_people = 0
         for user, _time in self.activity_cash.items():
             if _time[0] <= time:
