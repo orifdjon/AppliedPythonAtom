facd4fc96f3dbcb1abcadcb1bf0bbca43484a837
Arif <orif95@mail.ru>
2019-03-23 18:31:58 +0300

    done hw4

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -5,6 +5,13 @@ from multiprocessing import Process, Manager
 import os
 
 
+def count_words(path_to_dir, filename, queue):
+    with open(f'{path_to_dir}/{filename}', 'r', encoding='utf8') as f:
+        queue.put((filename, 0))
+        for words in f.readlines():
+            queue.put((filename, len([w for w in words.split(' ') if w != '\n'])))
+
+
 def word_count_inference(path_to_dir):
     '''
     Метод, считающий количество слов в каждом файле из директории
@@ -16,4 +23,25 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    tasks = []
+    for f in os.listdir(path_to_dir):
+        print(f)
+        p = Process(target=count_words, args=(path_to_dir, f, queue))
+        p.start()
+        tasks.append(p)
+
+    for task in tasks:
+        task.join()
+
+    hash_map = {}
+    len_of_queue = queue.qsize()
+    for _ in range(len_of_queue):
+        key, value = queue.get()
+        if key in hash_map:
+            hash_map[key] += value
+        else:
+            hash_map[key] = value
+    hash_map['total'] = sum(hash_map.values())
+    return hash_map
